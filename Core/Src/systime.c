ab595a53ca113b4bd03a8f2b16b731cd898363bc
two-dimensional-array <max.mazyr2012@gmail.com>
2022-04-25 00:53:53 +0300

    Source file systime.c is updated.

--- Core/Src/systime.c
+++ Core/Src/systime.c
@@ -4,5 +4,17 @@
  *  Created on: 25 апр. 2022 г.
  *      Author: HardCore
  */
+#include "systime.h"
+#include "stm32f1xx_hal.h"
 
+uint32_t Sys_Time_Get_Time(void)
+{
+  return HAL_GetTick();
+}
 
+bool Sys_Time_Its_Time(uint32_t start_time, uint32_t delay)
+{
+  uint32_t current_time = Sys_Time_Get_Time();
+  uint32_t delta = (current_time < start_time) ? ((UINT32_MAX - start_time) + current_time) : (current_time - start_time);
+  return (delta >= delay);
+}
