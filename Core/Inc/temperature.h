3d59a8cec8fd754df90510d51a32922a9a3385e9
two-dimensional-array <max.mazyr2012@gmail.com>
2022-04-25 00:06:33 +0300

    Header file temperature.h is updated.

--- Core/Inc/temperature.h
+++ Core/Inc/temperature.h
@@ -8,6 +8,10 @@
 #ifndef INC_TEMPERATURE_H_
 #define INC_TEMPERATURE_H_
 
+#define TEMPERATURE_BUFFER_SIZE 100
 
+float Get_Current_Temperature(void);
+void Measure_Temperature(void);
+void Get_Max_Middle_Min_Temperature(float* p_max_temperature, float* p_middle_temperature, float* p_min_temperature);
 
 #endif /* INC_TEMPERATURE_H_ */
