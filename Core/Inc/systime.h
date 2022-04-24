dadbc4d4446d0300bd73b4dd88321481336b96f4
two-dimensional-array <max.mazyr2012@gmail.com>
2022-04-25 00:53:38 +0300

    Header file systime.h is updated.

--- Core/Inc/systime.h
+++ Core/Inc/systime.h
@@ -8,6 +8,10 @@
 #ifndef INC_SYSTIME_H_
 #define INC_SYSTIME_H_
 
+#include <stdint.h>
+#include <stdbool.h>
 
+uint32_t Sys_Time_Get_Time(void);
+bool Sys_Time_Its_Time(uint32_t start_time, uint32_t delay);
 
 #endif /* INC_SYSTIME_H_ */
