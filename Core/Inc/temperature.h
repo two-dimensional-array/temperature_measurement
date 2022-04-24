ccdc9ccd8b5cd7a69cfbd4851aad515453ecf3d5
two-dimensional-array <max.mazyr2012@gmail.com>
2022-04-24 23:06:18 +0300

    Header file temperature.h is added.

---
+++ Core/Inc/temperature.h
@@ -0,0 +1,13 @@
+/*
+ * temperature.h
+ *
+ *  Created on: 24 апр. 2022 г.
+ *      Author: HardCore
+ */
+
+#ifndef INC_TEMPERATURE_H_
+#define INC_TEMPERATURE_H_
+
+
+
+#endif /* INC_TEMPERATURE_H_ */
