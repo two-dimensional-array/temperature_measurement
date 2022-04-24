3756403a301525885ce247b8c8a406b59fc32bc2
two-dimensional-array <max.mazyr2012@gmail.com>
2022-04-25 02:35:47 +0300

    Function Get_Current_Temperature() is optimized.

--- Core/Src/temperature.c
+++ Core/Src/temperature.c
@@ -6,28 +6,20 @@
  */
 #include "temperature.h"
 #include "dht11.h"
-#include <string.h>
+#include <stddef.h>
 
 static float temperature_buffer[TEMPERATURE_BUFFER_SIZE];  // Collection of 100 records temperature.
-static unsigned temperature_buffer_size = 0;  // Current size of collection.
+static unsigned temperature_buffer_current_index = 0;  // Current index of collection.
 
 float Get_Current_Temperature(void)
 {
-  return temperature_buffer[temperature_buffer_size];
+  return temperature_buffer[temperature_buffer_current_index];
 }
 
 void Measure_Temperature(void)
 {
-  if(temperature_buffer_size < TEMPERATURE_BUFFER_SIZE)
-  {
-    DHT11_Get_Data((temperature_buffer + temperature_buffer_size), NULL);
-    temperature_buffer_size++;
-  }
-  else
-  {
-    memcpy(temperature_buffer, (temperature_buffer + 1), ((TEMPERATURE_BUFFER_SIZE - 1) * sizeof(float)));
-    DHT11_Get_Data((temperature_buffer + TEMPERATURE_BUFFER_SIZE - 1), NULL);
-  }
+  temperature_buffer_current_index = (temperature_buffer_current_index < TEMPERATURE_BUFFER_SIZE) ? temperature_buffer_current_index + 1 : 0;
+  DHT11_Get_Data((temperature_buffer + temperature_buffer_current_index), NULL);
 }
 
 void Get_Max_Middle_Min_Temperature(float* p_max_temperature, float* p_middle_temperature, float* p_min_temperature)
