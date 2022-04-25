dfed71ef99b9a695b526ff44f3bf3b420feac7a1
two-dimensional-array <max.mazyr2012@gmail.com>
2022-04-25 09:18:14 +0300

    Function Get_Max_Middle_Min_Temperature() is optimized.

--- Core/Src/temperature.c
+++ Core/Src/temperature.c
@@ -24,22 +24,20 @@ void Measure_Temperature(void)
 
 void Get_Max_Middle_Min_Temperature(float* p_max_temperature, float* p_middle_temperature, float* p_min_temperature)
 {
-  float max_temperature = temperature_buffer[0];
-  float min_temperature = temperature_buffer[0];
-  float middle_temperature_buffer = temperature_buffer[0];
+  *p_max_temperature = temperature_buffer[0];
+  *p_middle_temperature = temperature_buffer[0];
+  *p_min_temperature = temperature_buffer[0];
   for (unsigned i = 1; i < TEMPERATURE_BUFFER_SIZE; i++)
   {
-    if(max_temperature > temperature_buffer[i])
+    if(*p_max_temperature > temperature_buffer[i])
     {
-      max_temperature = temperature_buffer[i];
+      *p_max_temperature = temperature_buffer[i];
     }
-    if(min_temperature < temperature_buffer[i])
+    if(*p_min_temperature < temperature_buffer[i])
     {
-      min_temperature = temperature_buffer[i];
+      *p_min_temperature = temperature_buffer[i];
     }
-    middle_temperature_buffer += temperature_buffer[i];
+    *p_middle_temperature += temperature_buffer[i];
   }
-  *p_max_temperature = max_temperature;
-  *p_middle_temperature = (middle_temperature_buffer / TEMPERATURE_BUFFER_SIZE);
-  *p_min_temperature = min_temperature;
+  *p_middle_temperature /= TEMPERATURE_BUFFER_SIZE;
 }
